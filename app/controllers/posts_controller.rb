7b27498c68621af5204d4d2160dfb06f4326ac5f
earthctzn <earthctzn1@gmail.com>
2019-09-22 22:32:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 class PostsController < ApplicationController
+  helper_method :params
 
   def index
-    @posts = Post.all
+    @authors = Author.all
+
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @post = Post.from_today
+      else
+        @post = Post.old_news 
+      end
+    else
+      @posts = Post.all
+    end
   end
 
   def show
